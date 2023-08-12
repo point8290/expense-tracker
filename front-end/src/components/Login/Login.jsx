a2166d9db21e6ec51879ca6f7915e37908fffe6a
Krishna Meghwal <krishnameghwal635@gmail.com>
2023-08-12 18:19:32 +0530

    Register Form

--- front-end/src/components/Login/Login.jsx
+++ front-end/src/components/Login/Login.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import Form from "../Form/Form";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import Button from "../Button/Button";
 function Login() {
   const onLogin = () => {};
   return (
     <Form className="loginForm" SubmitHandler={onLogin}>
       <div className="formTitle">Login</div>
       <div>
-        <label htmlFor="userEmail"> Email </label>
+        <label htmlFor="userEmail"> Enter your email </label>
         <input id="userEmail" type="text" />
       </div>
       <div>
-        <label htmlFor="userPassword"> Password </label>
+        <label htmlFor="userPassword">Enter your password </label>
         <input id="userPassword" type="password" />
       </div>
-      <div className="forgotPasswordLink">
+      <div className="formLink">
         <Link to="/forgot-passowrd"> Forgot password? </Link>
       </div>
-      <div>
-        <button>Submit</button>
-      </div>
+      <Button label="Submit" />
     </Form>
   );
 }
--- front-end/src/components/Register/Register.jsx
+++ front-end/src/components/Register/Register.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import Form from "../Form/Form";
+import Button from "../Button/Button";
+import { Link } from "react-router-dom";
+import "./Register.css";
 function Register() {
-  return <div>Register</div>;
+  const onRegister = () => {};
+  return (
+    <Form className="registerForm" SubmitHandler={onRegister}>
+      <div className="formTitle">Register</div>
+      <div>
+        <label htmlFor="userEmail"> Email </label>
+        <input id="userEmail" type="text" />
+      </div>
+      <div>
+        <label htmlFor="userMobile"> Mobile </label>
+        <input id="userMobile" type="text" />
+      </div>
+      <div>
+        <label htmlFor="userPassword"> Password </label>
+        <input id="userPassword" type="password" />
+      </div>
+      <div>
+        <label htmlFor="userConfirmPassword"> Confirm password </label>
+        <input id="userConfirmPassword" type="password" />
+      </div>
+      <div className="formLink">
+        <Link to="/forgot-passowrd"> Already have an account? </Link>
+      </div>
+      <div>
+        <Button label="Submit" />
+      </div>
+    </Form>
+  );
 }
 
 export default Register;
